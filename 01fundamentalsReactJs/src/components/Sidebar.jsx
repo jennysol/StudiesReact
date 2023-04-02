125c474aa5aa484ec987577acc520dc898ba56b5
jennysol <jennyssoliver@gmail.com>
2023-04-02 11:04:46 -0300

    feature: add Sidebar component

---
+++ 01fundamentalsReactJs/src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://images.unsplash.com/photo-1537151625747-768eb6cf92b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2568&q=80" 
+          alt="Imagem de perfil" 
+        />
+        <strong>Raimundo Afonso de Oliveira</strong>
+        <span>Web Developer Dog</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
