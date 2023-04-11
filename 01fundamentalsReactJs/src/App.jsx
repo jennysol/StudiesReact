fea3c9849de69d83f28d3368f4f5591c951eed07
jennysol <jennyssoliver@gmail.com>
2023-04-10 23:13:41 -0300

    Update: add post array prop in Post

--- 01fundamentalsReactJs/src/App.jsx
+++ 01fundamentalsReactJs/src/App.jsx
@@ -5,6 +5,39 @@ import { Sidebar } from './components/Sidebar';
 import './global.css';
 import styles from './App.module.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3362&q=80',
+      name: 'Raimundo Oliveira Afonso',
+      role: 'Web Developer Dog'
+    },
+    content: [
+      { type: 'paragraph', content: 'AU AU AU pessoal!'},
+      { type: 'paragraph', content: 'Descobri que sou mimado >.<.'},
+      { type: 'paragraph', content: '- Mundico.'},
+      { type: 'link', content: '#petisco'}
+    ],
+    publishedAt: new Date('2023-04-09 20:00:00')
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3362&q=80',
+      name: 'Cacau Emocionada',
+      role: 'Dev front-end'
+    },
+    content: [
+      { type: 'paragraph', content: 'AU dog peoples!'},
+      { type: 'paragraph', content: 'Minha humana n me alimenta de forma digna e eu estou com saudade do deus do parcao, vulgo, Raimundo.'},
+      { type: 'paragraph', content: '-- Atenciosamente, Cacau Emocionada.'},
+      { type: 'link', content: '#petiscoeharte'}
+    ],
+    publishedAt: new Date('2023-04-09 20:00:00')
+  },
+];
+
 function App() {
   return (
     <div>
@@ -13,7 +46,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post/>
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          }) }
         </main>
       </div>
     </div>
--- 01fundamentalsReactJs/src/components/Post.jsx
+++ 01fundamentalsReactJs/src/components/Post.jsx
@@ -1,29 +1,46 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import {Comment} from './Comment.jsx';
 import {Avatar} from './Avatar';
 
 import styles from './Post.module.css';
 
-export function Post() {
+
+export function Post({author, publishedAt, content}) {
+  const publishedDateFormat = format(publishedAt, "d 'de' LLL 'ás' HH:mm 'h' ", {locale: ptBR})
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3388&q=80"/>
+          <Avatar src={author.avatarUrl}/>
           
           <div className={styles.authorInfo}>
-            <strong>Cacau Francisca</strong>
-            <span>Back-end Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="02 de Abril ás 08:00h" dateTime="23-02-04">Publicado há uma hora</time>
+        <time title={publishedDateFormat} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        <p>AU AU AU pessoal!</p> 
-        <p>Minha humana n me alimenta de forma digna e eu estou com saudade do deus do parcao, vulgo, Raimundo.</p> 
-        <p>-- Atenciosamente, Cacau Francisca.</p>
-        <a href="#">#petisco</a>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>;
+          } 
+            else if (line.type === 'link') {
+            return <p><a href="#">{line.content}</a></p>;
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
