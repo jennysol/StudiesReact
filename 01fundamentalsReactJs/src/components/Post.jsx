3bee50fb6c45c72e7f6e2482573e7030c84cc24c
jennysol <jennyssoliver@gmail.com>
2023-04-11 11:20:39 -0300

    feature: add valid form

--- 01fundamentalsReactJs/src/components/Post.jsx
+++ 01fundamentalsReactJs/src/components/Post.jsx
@@ -30,7 +30,12 @@ export function Post({author, publishedAt, content}) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo não pode estar em branco!')
   }
 
   function deleteComment(commentToDelete) {
@@ -40,6 +45,8 @@ export function Post({author, publishedAt, content}) {
 
     setComments(commentsWithoutDeleteOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -77,10 +84,17 @@ export function Post({author, publishedAt, content}) {
           placeholder='Deixe um comentário'
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type='submit'>Comentar</button>
+          <button 
+            type='submit' 
+            disabled={isNewCommentEmpty}
+          >
+            Comentar
+          </button>
         </footer>
       </form>
 
