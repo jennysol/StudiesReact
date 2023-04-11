307d3872a72ef6034567b2ae3568d9dd17effcd9
jennysol <jennyssoliver@gmail.com>
2023-04-10 23:37:33 -0300

    update: add initial use state in comment post

--- 01fundamentalsReactJs/src/components/Post.jsx
+++ 01fundamentalsReactJs/src/components/Post.jsx
@@ -1,5 +1,6 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
 import {Comment} from './Comment.jsx';
 import {Avatar} from './Avatar';
@@ -8,12 +9,20 @@ import styles from './Post.module.css';
 
 
 export function Post({author, publishedAt, content}) {
+  const [comments, setComments] = useState([1, 2 ])
+
   const publishedDateFormat = format(publishedAt, "d 'de' LLL 'ás' HH:mm 'h' ", {locale: ptBR})
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
     addSuffix: true
   })
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    setComments([...comments, comments.length + 1]);
+  }
 
   return (
     <article className={styles.post}>
@@ -43,8 +52,9 @@ export function Post({author, publishedAt, content}) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
+
         <textarea
           placeholder='Deixe um comentário'
         />
@@ -55,8 +65,9 @@ export function Post({author, publishedAt, content}) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment/>
-        <Comment/>
+        {comments.map(comment => {
+          return <Comment/>
+        })}
       </div>
     </article>
   );
