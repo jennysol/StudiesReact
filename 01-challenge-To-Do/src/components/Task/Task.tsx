688943dc10e6c2a7933f48744318487bd15bc785
jennysol <jennyssoliver@gmail.com>
2023-04-14 18:05:56 -0300

    update: task component with funcionalides

--- 01-challenge-To-Do/src/components/Task/Task.tsx
+++ 01-challenge-To-Do/src/components/Task/Task.tsx
@@ -1,15 +1,34 @@
+import { Trash, CheckCircle } from "phosphor-react";
+import { ITasks } from "../../App";
 
 import styles from './Task.module.css'
 
-export function Task() {
+export interface TaskProps {
+  task: ITasks;
+  onDelete: (taskId: string) => void;
+  onComplete: (taskId: string) => void;
+}
+
+export function Task({ task, onDelete, onComplete }: TaskProps) {
+
   return (
-    <div className="container">
-      <header>
-        <form className={styles.taskForm}>
-          <input type="text" placeholder="Adicione uma nova Tarefa"/>
-          <button type='submit'>Criar</button>
-        </form>
-      </header>
-    </div>
+    <li className={styles.task} >
+      <button 
+        className={styles.checkContainer}
+        onClick={() => onComplete(task.id)}
+        >
+        { task.isCompleted ? <CheckCircle color="#8284FA" size={22} /> : <div id="radio-input"/>}
+      </button>
+
+      <p className={task.isCompleted ? styles.textCompleted : ""}>{task.title}</p>
+
+      <button 
+        type='submit' 
+        className={styles.deleteButton}
+        onClick={() => onDelete(task.id)}
+      >
+        <Trash size={20} />
+      </button>
+    </li>
   );
 }
