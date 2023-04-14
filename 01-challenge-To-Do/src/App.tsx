086369e09e2f7f0ff86deab4f0a2203f526e1d3c
jennysol <jennyssoliver@gmail.com>
2023-04-14 18:06:31 -0300

    update: add functions in App

--- 01-challenge-To-Do/src/App.tsx
+++ 01-challenge-To-Do/src/App.tsx
@@ -1,17 +1,77 @@
 
 import { Header } from './components/Header/Header'
-import { Task } from './components/Task/Task';
+import { TaskList } from './components/TaskList/TaskList';
 
 import './global.css';
 import './App.css'
+import { useEffect, useState } from 'react';
 
+const LOCAL_STORAGE_KEY = "todo.savedTasks";
+export interface ITasks {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
 
 function App() {
+  const [tasks, setTasks] = useState<ITasks[]>([]);
+
+  function loadSavedTask() {
+    const saved = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if(saved) {
+      setTasks(JSON.parse(saved))
+    }
+  }
+
+  useEffect(() => {
+    loadSavedTask();
+  }, []);
+
+  function setTasksAndSave(newTasks: ITasks[]) {
+    setTasks(newTasks);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newTasks))
+  }
+
+  function addTask(taskTitle: string) {
+    setTasksAndSave([
+      ...tasks,
+      {
+        id: crypto.randomUUID(),
+        title: taskTitle,
+        isCompleted: false
+      }
+    ])
+  }
+
+  function deleteTask(taskId: string) {
+    const newTasks = tasks.filter((task) => task.id != taskId);
+    setTasksAndSave(newTasks);
+  }
+
+  function toggleTaskCompleted(taskId: string) {
+    const newTasks = tasks.map((task) => {
+      if (task.id == taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted
+        }
+      }
+      return task
+    });
+
+    setTasksAndSave(newTasks);
+  }
+
   return (
     <div>
      <Header/>
      <main>
-      <Task/>
+      <TaskList
+        tasks={tasks}
+        onDeleteTask={deleteTask}
+        onAddTask={addTask}
+        onComplete={toggleTaskCompleted}
+      />
      </main>
     </div>
   )
