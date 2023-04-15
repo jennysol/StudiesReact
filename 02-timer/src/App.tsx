cc31438cca32ec5bda8f3b579005eb2ba6ff3bd4
jennysol <jennyssoliver@gmail.com>
2023-04-14 22:26:24 -0300

    feature: add router dom

--- 02-timer/src/App.tsx
+++ 02-timer/src/App.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <div>
       <ThemeProvider theme={defaultTheme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
         <GlobalStyle />
       </ThemeProvider>
     </div>
