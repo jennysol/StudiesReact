04f6756625a605311da886bb289b3f47eaee9074
jennysol <jennyssoliver@gmail.com>
2023-04-14 22:11:24 -0300

    feature: add styled components

--- 02-timer/src/App.tsx
+++ 02-timer/src/App.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <div>
-
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+      </ThemeProvider>
     </div>
   )
 }
