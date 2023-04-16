7659a9c00048b4130fe1e1383a9ca96522a1f68b
jennysol <jennyssoliver@gmail.com>
2023-04-16 19:57:18 -0300

    feature: add typescript in form

--- 02-timer/src/pages/Home/index.tsx
+++ 02-timer/src/pages/Home/index.tsx
@@ -21,12 +21,18 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
 })
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 5,
+    },
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log('create new cycle', data)
   }
 
