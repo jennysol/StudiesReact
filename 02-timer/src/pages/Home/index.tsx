b88fc310b2d8fee9921078a5b39b9330e628efef
jennysol <jennyssoliver@gmail.com>
2023-04-16 16:53:49 -0300

    update: styles in inputs

--- 02-timer/src/pages/Home/index.tsx
+++ 02-timer/src/pages/Home/index.tsx
@@ -15,13 +15,25 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput type="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput
+            type="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos</span>
