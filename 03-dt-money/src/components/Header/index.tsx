38f219127c5d5eb99537b8147a834bb650789862
jennysol <jennyssoliver@gmail.com>
2023-04-19 02:28:19 -0300

    feature: add Header component

---
+++ 03-dt-money/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import logoImg from '../../assets/logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} />
+        <NewTransactionButton>Nova transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
