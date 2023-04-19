bc4fee5b8fb7e635b270389aa29dd2040d874c13
jennysol <jennyssoliver@gmail.com>
2023-04-19 01:55:13 -0300

    feature: add global styled component

--- 03-dt-money/src/App.tsx
+++ 03-dt-money/src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>Hi</h1>
-    </div>
+    </ThemeProvider>
+      
   )
 }
