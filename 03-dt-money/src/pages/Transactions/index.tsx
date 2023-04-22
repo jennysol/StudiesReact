2e83215c283a387bc64d544832ec524c0b97652b
jennysol <jennyssoliver@gmail.com>
2023-04-22 10:49:46 -0300

    feature: add table in Transactions

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
+import { PriceHightLight, TransactionsContainer, TransactionsTable } from "./styles"
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+     <TransactionsContainer>
+       <TransactionsTable>
+        <tbody>
+          <tr>
+            <td width="50%">Desenvolvimento de Site</td>
+            <td>
+              <PriceHightLight variant="income">
+                R$ 12.000,00
+              </PriceHightLight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+          <tr>
+            <td width="50%">Chocolate</td>
+            <td>
+              <PriceHightLight variant="outcome">
+                - R$ 50.00
+              </PriceHightLight>
+            </td>
+            <td>Alimento</td>
+            <td>13/04/2022</td>
+          </tr>
+        </tbody>
+      </TransactionsTable>
+     </TransactionsContainer>
     </div>
   )
 }
