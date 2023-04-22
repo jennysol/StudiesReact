51c03a19507efc16644b4c3e4a41d5831fe3cf67
jennysol <jennyssoliver@gmail.com>
2023-04-22 15:12:53 -0300

    update: add entrance and exit button

--- 03-dt-money/src/components/NewTransactionModal/styles.ts
+++ 03-dt-money/src/components/NewTransactionModal/styles.ts
@@ -67,5 +67,33 @@ export const CloseButton = styled(Dialog.Close)`
   right: 1.5rem;
   line-height: 0;
   cursor: pointer;
-  color: ${(props) => props.theme["gray-500"]};;
+  color: ${(props) => props.theme["gray-500"]};
 `
+interface TransactionTypeProps {
+  variant: 'income' | 'outcome'
+}
+
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
+`
+
+export const TransactionTypeButton = styled.button<TransactionTypeProps>`
+  background: ${(props) => props.theme["gray-700"]};
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 0;
+  color: ${(props) => props.theme["gray-300"]};
+
+  svg {
+    color: ${props => props.variant === 'income' ? props.theme["green-500"] : props.theme["red-300"]};
+  }
+`
+
