ff4626ad7a3620364fc8bde2fb5e35d5eeb13d68
jennysol <jennyssoliver@gmail.com>
2023-04-22 23:49:48 -0300

    feature: add req to transactions api

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react"
 import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
 import { SearchForm } from "./components/SearchForm"
 import { PriceHightLight, TransactionsContainer, TransactionsTable } from "./styles"
 
+interface Transaction {
+  id: number,
+  description: string,
+  type: 'income' | 'outcome'
+  price: number
+  category: string
+  createdAt: string
+}
+
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3000/transactions')
+    const data = await response.json()
+
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, [])
+
+
   return (
     <div>
       <Header />
@@ -13,26 +37,20 @@ export function Transactions() {
       <SearchForm />
        <TransactionsTable>
         <tbody>
-          <tr>
-            <td width="50%">Desenvolvimento de Site</td>
-            <td>
-              <PriceHightLight variant="income">
-                R$ 12.000,00
-              </PriceHightLight>
-            </td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
-          <tr>
-            <td width="50%">Chocolate</td>
-            <td>
-              <PriceHightLight variant="outcome">
-                - R$ 50.00
-              </PriceHightLight>
-            </td>
-            <td>Alimento</td>
-            <td>13/04/2022</td>
-          </tr>
+          {transactions.map(transaction => {
+            return (
+              <tr key={transaction.id}>
+                <td width="50%">{transaction.description}</td>
+                <td>
+                  <PriceHightLight variant={transaction.type}>
+                    {transaction.price}
+                  </PriceHightLight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            )
+          })}
         </tbody>
       </TransactionsTable>
      </TransactionsContainer>
