d2b219d67330cebf6e6ccc03a86631a90abf60bf
jennysol <jennyssoliver@gmail.com>
2023-04-22 23:05:28 -0300

    feature: add radio group

--- 03-dt-money/src/components/NewTransactionModal/styles.ts
+++ 03-dt-money/src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,6 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import * as Dialog from '@radix-ui/react-dialog'
+import * as RadioGroup from '@radix-ui/react-radio-group'
 
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
@@ -73,7 +74,7 @@ interface TransactionTypeProps {
   variant: 'income' | 'outcome'
 }
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
@@ -94,6 +95,20 @@ export const TransactionTypeButton = styled.button<TransactionTypeProps>`
 
   svg {
     color: ${props => props.variant === 'income' ? props.theme["green-500"] : props.theme["red-300"]};
+  }
+
+  &[data-state='unchecked']:hover {
+    transition: background-color 0.2s;
+    background: ${props => props.theme['gray-600']};
+  }
+
+  &[data-state='checked'] {
+    color: ${props => props.theme.white};
+    background: ${props => props.variant === 'income' ? props.theme['green-300'] : props.theme['red-500']};
+
+    svg {
+      color: ${props => props.theme.white};
+    }
   }
 `
 
