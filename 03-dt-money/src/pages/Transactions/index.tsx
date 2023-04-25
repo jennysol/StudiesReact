f1027d2709e2910bb776f207fe8e0cf369bbc839
jennysol <jennyssoliver@gmail.com>
2023-04-25 18:52:51 -0300

    feature: add formatters

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -4,6 +4,7 @@ import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
 import { SearchForm } from "./components/SearchForm"
 import { PriceHightLight, TransactionsContainer, TransactionsTable } from "./styles"
+import { priceFormatter, dateFormatter } from '../../utils/formatter'
 
 export function Transactions() {
   const { transactions } = useContext(TransactionsContext)
@@ -23,11 +24,12 @@ export function Transactions() {
                 <td width="50%">{transaction.description}</td>
                 <td>
                   <PriceHightLight variant={transaction.type}>
-                    {transaction.price}
+                    {transaction.type === 'outcome' && '-  '}
+                    { priceFormatter.format(transaction.price)}
                   </PriceHightLight>
                 </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{dateFormatter.format(new Date (transaction.createdAt))}</td>
               </tr>
             )
           })}
