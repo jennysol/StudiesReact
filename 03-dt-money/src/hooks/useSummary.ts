8f725de6b179b08cd6ebfad265c48b1058bd0900
jennysol <jennyssoliver@gmail.com>
2023-04-29 21:17:00 -0300

    fix: import Transaction Context

--- 03-dt-money/src/hooks/useSummary.ts
+++ 03-dt-money/src/hooks/useSummary.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { TransactionsContext } from '../src/contexts/TransactionsContext'
+import { TransactionsContext } from '../contexts/TransactionsContext'
 
 export function useSummary() {
   const { transactions } = useContext(TransactionsContext)
